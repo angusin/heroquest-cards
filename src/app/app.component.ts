ed6c55900c0e7ded5d006ef19ba23324f5b23391
Miguel Viñas <m.vinas@crowdfarming.com>
2024-10-14 18:24:58 +0200

    feat: first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FlowbiteService } from './core/services/flowbite.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'heroquest-cards';
+export class AppComponent implements OnInit {
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite(() => {
+      //console.log('Flowbite loaded', flowbite);
+    });
+  }
 }
