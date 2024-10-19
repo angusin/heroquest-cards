cfda8d54f6bf01db1e531ef1ab98c0b4b852e064
Miguel Viñas <m.vinas@crowdfarming.com>
2024-10-19 10:19:29 +0200

    feat: create basic layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { FlowbiteService } from './core/services/flowbite.service';
+import { NavbarComponent } from './components/layout/navbar/navbar.component';
+import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { MainContainerComponent } from './components/layout/main-container/main-container.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavbarComponent, SidebarComponent, MainContainerComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
