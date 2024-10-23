fe7f42a02720478753c7ec2afd77bb4950a44c40
Miguel Viñas <m.vinas@crowdfarming.com>
2024-10-23 08:29:43 +0200

    feat: navigate to detail page

--- src/app/components/shared/card-container/card-container.component.ts
+++ src/app/components/shared/card-container/card-container.component.ts
@@ -2,11 +2,12 @@ import { Component, Input } from '@angular/core';
 import { Card } from '../../../core/interfaces/common';
 import { CardTypeTagComponent } from '../card-type-tag/card-type-tag.component';
 import { FavoriteButtonComponent } from '../favorite-button/favorite-button.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-card-container',
   standalone: true,
-  imports: [CardTypeTagComponent, FavoriteButtonComponent],
+  imports: [CardTypeTagComponent, FavoriteButtonComponent, RouterModule],
   templateUrl: './card-container.component.html',
   styleUrl: './card-container.component.scss',
 })
