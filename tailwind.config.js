d473e81ba4f53eb84f65f897a8dd952d115bd626
Miguel Viñas <m.vinas@crowdfarming.com>
2025-04-01 18:06:31 +0200

    feat: change colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,7 @@ module.exports = {
           accent: '#F7C64B', // Brighter yellow for hover states
           'dark-800': '#1F1F1F', // Slightly lighter dark for cards
           'dark-700': '#2B2B2B', // Even lighter dark for hover states
-          'dark-600': '#343434', // For borders and dividers
+          'dark-600': '#464646', // For borders and dividers
           'light-200': '#CCCCCC', // Dimmer text
           'light-300': '#A3A3A3', // Even dimmer text
           'light-400': '#737373', // For less important text
